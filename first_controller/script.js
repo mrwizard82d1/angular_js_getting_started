4a50d2f50c65fbc13fa89f2733742ea25b328b76
Larry Jones <larryj@blinds.com>
2017-01-28 16:38:25 -0600

    First controller - but still an error
    The Pluralsight tutorial uses a function in the global scope as the
    controller. However, an overlay on the video indicates that this
    structure is no longer allowed beginning, I believe with Angular 1.2.5.
    This StackOverflow post,
    http://stackoverflow.com/questions/25111831/controller-not-a-function-got-undefined-while-defining-controllers-globally,
    indicates that this construct was not allowed as of Angular 1.3.
    
    The tutorial indicates that Scott Allen will describe later how to
    expose the controller function is a module using an IIFE; however, the
    StackOverflow post also describes how to register the function. For now,
    I will use the StackOverflow approach. (Based on previous tutorials, I
    believe this is the "accepted" approach.)

--- first_controller/script.js
+++ first_controller/script.js
@@ -1,2 +1,21 @@
 // Code goes here
 
+/*
+ * AngularJS invokes a function with the same name as the value of the `ng-controller` attribute on an HTML tag. 
+ *
+ * AngularJS invokes this function with a single parameter, `$scope`, (yes, this is a valid JavaScript variable name).
+ * The value of this parameter is the JavaScript scope (environment) used when evaluating AngularJS binding expressions
+ * in the scope of the tag containing the `ng-controller="MainController"` attribute.
+ */
+var MainController = function($scope) {
+
+}
+
+/*
+ * When you attempt to load this page, it **still** fails. Beginning with Angular 1.3, the framework will no longer
+ * search the global scope. For details, see this post on StackOverflow:
+ * http://stackoverflow.com/questions/25111831/controller-not-a-function-got-undefined-while-defining-controllers-globally
+ *
+ * In addition to explaining the change to the framework, this post also describes how to register the function as a
+ * controller. 
+ */

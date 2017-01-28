7763dc781274c033c68aa4e75dbc0f75957a9759
Larry Jones <larryj@blinds.com>
2017-01-28 10:50:28 -0600

    Script demonstrating functional abstractions
    This script creates a function value, assigns it to a variable and then
    invokes the (function) value bound to that variable.

--- hello_angular/script.js
+++ hello_angular/script.js
@@ -1,27 +1,18 @@
 // Code goes here
 
-// Imagine a situation in which an error is occurring on the page: the
-// alert back shows '5' instead of the expected '4'. Use the Developer
-// tools to diagnose the error.
+/*
+ * This script illustrates using functions as abstractions.
+ */
+
+// Begin by defining a function to do "real" work.
+var work = function() {
+  console.log("working hard!")
+}
+
+// The preceeding assignment **does not** actually invoke the function.
+// The `function` keyword simply **defines** a value to be a function;
+// that is, something one can invoke like `work()`.
 //
-// The general process:
-// * Open the Developer Tools windows
-// * Select the "Sources" window
-// * Search for the file of interest (in this case `script.js`). (Yes,
-//   this process assumes you have some idea about the file of
-//   interest.) 
-// * Put a break point on the line of interest. 
-//   * Click to the left of the line number. A blue indicator will
-//   appear.
-// * Refresh the page. 
-// * When it stops at the breakpoint
-//   * Hover over variables of interest to display their values.
-//     * Observe that `result` is 5
-//     * Note that `result` is 2 + `operand`
-//     * Observe that `operand` is 3
-//     * Voila! You've found the problem.
-// * Correct the issue and refresh the page
-// * Remember that `F8` will continue execution.
-var operand = 3;
-var result = 2 + operand;
-alert(result);
+// To actually invoke the function we defined, one must invoke or call
+// it.
+work();

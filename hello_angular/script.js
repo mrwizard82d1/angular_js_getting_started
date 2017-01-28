3f27c3cc377dcbedb108047e67246e9a4cb46402
Larry Jones <larryj@blinds.com>
2017-01-28 11:32:56 -0600

    The "revealing module pattern"
    This script illustrates the revealing module pattern.

--- hello_angular/script.js
+++ hello_angular/script.js
@@ -1,53 +1,52 @@
 // Code goes here
 
 /*
- * This script illustrates using functions as abstractions.
+ * This script is an example of the "revealing module pattern."
+ *
+ * In this pattern, one **does not** write functions inline; instead,
+ * one writes functions that return objects (the "module") containing
+ * the functions exposed by the modules.
  */
-
-// Begin by defining a function to do "real" work.
-var work = function() {
-	console.log("Working hard. Oops!");
-	// Our "real work" now raises an exception.
-	console.log(foo);
-}
 
 /*
- * AngularJS uses functions to abstract a framework. Just like an
- * object-oriented framework often uses abstract functions to define
- * placeholders for functions defined by derived classes, AngularJS (and
- * other JavaScript packages) use higher-level functions, for example,
- * functions taking functions as arguments, as a placeholder. I, the
- * consumer of the framework/package/module, define a function to do the
- * work needed by my application. I then pass that function as an
- * argument to a framework function.
- *
+ * The function, `createWorker`, is a function returning an object (a
+ * module) that contains the behavior exposed by our API.
  */
-var doWork = function(f) {
+var createWorker = function() {
 
-	// Notice that the framework function invokes the application
-	// function in the midst of doing other work. In this case, it
-	// simply logs the fact that it is starting work and when work ends.
-	// Additionally, it wraps the supplied function in a `try-catch`
-	// block, perhaps to provide a common mechanism to handle errors.
-
-	console.log("Starting to do work.")
-
-	try {
-		f();
-	} catch (ex) {
-		// However, the `try-catch` block prevents the exception raised
-		// by `work` from propogating up to the browser.
-		console.log(ex);
+	/*
+	 * `task1` is the name of an **internal** function. It is **not**
+	 * exposed to consumers of the returned value.
+	 */
+	var task1 = function() {
+		console.log("task1");
 	}
 
-	console.log("Ending the work to be done.")
+	/*
+	 * Similarly, `task2` is another internal function. Because of
+	 * JavaScript scoping rules, its name is only available in the scope
+	 * of `createWorker` (and any enclosed scopes).
+	 */
+	var task2 = function() {
+		console.log("task2");
+	}
 
+	/*
+	 * Finally, we return an object. The returned object exposes two
+	 * members, `job1` and `job2` which are bound to the internal
+	 * functions that do our work. 
+	 */
+	return {
+		job1: task1,
+		job2: task2
+	}
 }
 
-/*
- * Finally, at some later time, the framework invokes its function (in
- * this example, `doWork`). This invocation performs the work of the
- * framework *in addition to* the application work performed by `work`.
- */
-doWork(work);
+var worker = createWorker();
 
+/*
+ * We invoke `task1` and `task2`, from the global scope but we
+ * "see" neither `task1 nor `task2`. But we do see their result. :)
+ */
+worker.job1();
+worker.job2();

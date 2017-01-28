e8538c2376e0c26193e96be349b5418f6d0fc4a6
Larry Jones <larryj@blinds.com>
2017-01-28 16:54:32 -0600

    Register `MainController` with AngularJS
    This registration occurs using the `controller` method of the
    `angular.module` function.

--- first_controller/script.js
+++ first_controller/script.js
@@ -12,10 +12,15 @@ var MainController = function($scope) {
 }
 
 /*
- * When you attempt to load this page, it **still** fails. Beginning with Angular 1.3, the framework will no longer
- * search the global scope. For details, see this post on StackOverflow:
- * http://stackoverflow.com/questions/25111831/controller-not-a-function-got-undefined-while-defining-controllers-globally
+ * I'm uncertain of the purpose of the `inject` call. I believe this function adds the variable `$scope` to the
+ * controller when I register a controller function defined **outside** the scope of `angular.module`.
  *
- * In addition to explaining the change to the framework, this post also describes how to register the function as a
- * controller. 
+ * After injecting the `$scope` variable, I register my function with the Angular application (`first-controller`).
+ * This registration requires adding a value, `first-controller` to the `ng-app` attribute in my page. Additionally,
+ * this registration allows me more indirection, I must use the name, a string, "MainController" because that name
+ * agrees with the value of the `ng-controller` attribute defined on the `body` element; however, I could then name the
+ * function whatever I wanted. (I'm uncertain if I could use an anonymous function if I had defined the controller
+ * function inside the `angular.module` function.)
  */
+MainController.inject = ['$scope'];
+angular.module('first-controller', []).controller('MainController', MainController);

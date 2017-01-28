cc3775971839777a56daa81b953500322a031a35
Larry Jones <larryj@blinds.com>
2017-01-28 13:13:39 -0600

    A **single** global name
    We again take advantage of function scopes by enclosing all our work in
    a **single** function. When we invoke that single function, we perform
    all the work that we previously perform.
    
    However, we still add a single name to the global scope - with all the
    concomitant issues. :(

--- hello_angular/script.js
+++ hello_angular/script.js
@@ -20,72 +20,74 @@
  */
 
 /*
- * This script is an example of the "revealing module pattern."
- *
- * In this pattern, one **does not** write functions inline; instead,
- * one writes functions that return objects (the "module") containing
- * the functions exposed by the modules.
+ * Adding an enclosing scope can reduce **many* global names to a single
+ * name. 
  */
+var program = function() {
+	var createWorker = function() {
 
-/*
- * The function, `createWorker`, is a function returning an object (a
- * module) that contains the behavior exposed by our API.
- */
-var createWorker = function() {
+		/*
+		 * Remember that any variables defined in this function are
+		 * only visible within this function; that is, they are private. But
+		 * they **are** visible to functions defined within this function.
+		 */
+		var workerCount = 0;
 
-	/*
-	 * Remember that any variables defined in this function are
-	 * only visible within this function; that is, they are private. But
-	 * they **are** visible to functions defined within this function.
-	 */
-	var workerCount = 0;
+		/*
+		 * `task1` is the name of an **internal** function. It is **not**
+		 * exposed to consumers of the returned value.
+		 *
+		 * `task1` increments the local (private) variable `workerCount` and
+		 * then logs its value.
+		 */
+		var task1 = function() {
+			workerCount++;
+			console.log("task1", workerCount);
+		}
 
-	/*
-	 * `task1` is the name of an **internal** function. It is **not**
-	 * exposed to consumers of the returned value.
-	 *
-	 * `task1` increments the local (private) variable `workerCount` and
-	 * then logs its value.
-	 */
-	var task1 = function() {
-		workerCount++;
-		console.log("task1", workerCount);
+		/*
+		 * Similarly, `task2` is another internal function. Because of
+		 * JavaScript scoping rules, its name is only available in the scope
+		 * of `createWorker` (and any enclosed scopes).
+		 *
+		 * Similarly, `task2` increments the private variable `workerCount`.
+		 *
+		 */
+		var task2 = function() {
+			workerCount++;
+			console.log("task2", workerCount);
+		}
+
+		/*
+		 * Finally, we return an object. The returned object exposes two
+		 * members, `job1` and `job2` which are bound to the internal
+		 * functions that do our work. 
+		 */
+		return {
+			job1: task1,
+			job2: task2
+		}
 	}
 
-	/*
-	 * Similarly, `task2` is another internal function. Because of
-	 * JavaScript scoping rules, its name is only available in the scope
-	 * of `createWorker` (and any enclosed scopes).
-	 *
-	 * Similarly, `task2` increments the private variable `workerCount`.
-	 *
-	 */
-	var task2 = function() {
-		workerCount++;
-		console.log("task2", workerCount);
-	}
+	var worker = createWorker();
 
 	/*
-	 * Finally, we return an object. The returned object exposes two
-	 * members, `job1` and `job2` which are bound to the internal
-	 * functions that do our work. 
+	 * We invoke `task1` and `task2`, from the global scope but we
+	 * "see" neither `task1 nor `task2`. But we do see their result. :)
+	 *
+	 * Invoking `job1` and `job2` increments the variable `workerCount`
+	 * which is only local to the `createWorker` function.
 	 */
-	return {
-		job1: task1,
-		job2: task2
-	}
+	worker.job1();
+	worker.job2();
+	worker.job2();
+	worker.job2();
 }
 
-var worker = createWorker();
-
 /*
- * We invoke `task1` and `task2`, from the global scope but we
- * "see" neither `task1 nor `task2`. But we do see their result. :)
- *
- * Invoking `job1` and `job2` increments the variable `workerCount`
- * which is only local to the `createWorker` function.
+ * We now invoke a **single** function to do all our work. Notice that
+ * our script still has the same **behavior** as the previous versions;
+ * however, it "pollutes" the global scope far, far less because it only
+ * defines a single, global name.
  */
-worker.job1();
-worker.job2();
-worker.job2();
-worker.job2();
+program();

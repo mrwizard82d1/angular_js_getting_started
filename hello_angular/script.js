bae4c64830873527b5329abbc3107977d9717794
Larry Jones <larryj@blinds.com>
2017-01-28 11:02:25 -0600

    How frameworks use functions as abstractions
    This script describes, in its comments, how JavaScript
    packages/frameworks/modules use functions as abstractions. In this
    example, a framework performs common work (the majority of `doWork`),
    but invokes application-specific work (`work`) at an appropriate time.

--- hello_angular/script.js
+++ hello_angular/script.js
@@ -9,10 +9,41 @@ var work = function() {
   console.log("working hard!")
 }
 
-// The preceeding assignment **does not** actually invoke the function.
-// The `function` keyword simply **defines** a value to be a function;
-// that is, something one can invoke like `work()`.
-//
-// To actually invoke the function we defined, one must invoke or call
-// it.
-work();
+/*
+ * AngularJS uses functions to abstract a framework. Just like an
+ * object-oriented framework often uses abstract functions to define
+ * placeholders for functions defined by derived classes, AngularJS (and
+ * other JavaScript packages) use higher-level functions, for example,
+ * functions taking functions as arguments, as a placeholder. I, the
+ * consumer of the framework/package/module, define a function to do the
+ * work needed by my application. I then pass that function as an
+ * argument to a framework function.
+ *
+ */
+var doWork = function(f) {
+
+	// Notice that the framework function invokes the application
+	// function in the midst of doing other work. In this case, it
+	// simply logs the fact that it is starting work and when work ends.
+	// Additionally, it wraps the supplied function in a `try-catch`
+	// block, perhaps to provide a common mechanism to handle errors.
+
+	console.log("Starting to do work.")
+
+	try {
+		f();
+	} catch (ex) {
+		console.log(ex);
+	}
+
+	console.log("Ending the work to be done.")
+
+}
+
+/*
+ * Finally, at some later time, the framework invokes its function (in
+ * this example, `doWork`). This invocation performs the work of the
+ * framework *in addition to* the application work performed by `work`.
+ */
+doWork(work);
+

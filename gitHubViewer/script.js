c771bff97f5cad89ebf31809ca72ff95091def4b
Larry Jones <larryj@blinds.com>
2017-01-29 07:22:14 -0600

    Change calls to Angular to address minification
    Most production JavaScript code undergoes minification before being
    transmitted over the wire. One typical result of this minification is
    changing the names of variables from meaningful names like `$scope` to
    meaningless but shorter names like `n`.
    
    Angular, when invoking client code like controllers, looks for the
    meaningful names (`$scope`) in the parameter list of the controllers.
    When it finds the name `n`, Angular decides **not** to pass `$scope` as
    an actual controller argument. The application then fails.
    
    To address this issue, Angular provides a registration syntax for
    controllers that is robust in the face of minification. This syntax
    involves an array as the second argument to `controller`. The array
    contains the string names of arguments, in the correct order, that
    Angular must pass to the controller invocation even if it **did not**
    find meaningful parameter names; that is, `$scope`.

--- gitHubViewer/script.js
+++ gitHubViewer/script.js
@@ -42,8 +42,24 @@
 	/*
 	 * Having defined my controller, the value referenced by the variable `MainController`, I now register that
 	 * controller with the application.
+	 *
+	 * For production code, I need to make one additional change. Most production code preprocesses the JavaScript sent
+	 * the browser using minification. This preprocessing reduces the page download size; however, a "side-effect" is to
+	 * change the names of local variables. For example, `$scope` might become the single letter 'n'. Although this
+	 * change is intended by the minifier, when Angular finds the controller, it will look discover that it need **not**
+	 * inject `$scope` and `$http` (because those named parameters are **not** arguments to `MainController`). But `n`
+	 * and, for example, `x` are. Because we do not inject `$scope` and `$http`, the application will fail. 
+	 *
+	 * To address this issue, Angular has an alternative syntax for registering a controller: instead of simply passing
+	 * the controller function, `MainController`, as the second argument to `controller`, pass an array. The last
+	 * argument of the array is **still** `MainController`; however, preceeding arguments are the string names of the
+	 * variables that AngularJS must pass when invoking the controller - even if it finds no parameters named `$scope`
+	 * and `$http` in the function definition.
+	 *
+	 * (I think this reasoning might apply to other constructs I observed previously where we seemed to supply
+	 * "meaningless" strings with the same values as the names of meaningfull parameters.)
 	 */
-	app.controller('MainController', MainController);
+	app.controller('MainController', ['$scope', '$http', MainController]);
 }());
 
 /*

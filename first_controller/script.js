70d978e1f4664a7f87aa8cab4321f60453a67776
Larry Jones <larryj@blinds.com>
2017-01-28 19:58:24 -0600

    Bind object to Angular controller
    This commit exposes both a single value, `message`, and a complex value
    (a JavaScript object), `person`, to the controller.
    
    The controller then uses this object to not only bind textual values,
    `salutation` and `lastName`, but to bind to attribute values of an `img`
    tag.
    
    Rendering the page produces an error because when the page first
    renders, Angular is not fully loaded. However, I will fix this issue in
    the next commit.

--- first_controller/script.js
+++ first_controller/script.js
@@ -1,19 +1,34 @@
 // Code goes here
 
 /*
- * AngularJS invokes a function with the same name as the value of the `ng-controller` attribute on an HTML tag. 
- *
- * AngularJS invokes this function with a single parameter, `$scope`, (yes, this is a valid JavaScript variable name).
- * The value of this parameter is the JavaScript scope (environment) used when evaluating AngularJS binding expressions
- * in the scope of the tag containing the `ng-controller="MainController"` attribute.
+ * Time to display not just simple data, but data from an object.
  */
 var MainController = function($scope) {
+	// Define a new, private object, bound to `person`
+	var person = {
+		salutation: "Mr.",
+		lastName: "Wizard",
+		imageSource: "mrwizard_1.jpeg"
+	};
+
 	$scope.message = "Hello, Angular World!";
+	// Add the internal (private) `person` object to the scope of the controller
+	$scope.person = person;
 }
 
 MainController.inject = ['$scope'];
 angular.module('first-controller', []).controller('MainController', MainController);
 
 /*
- * Eureka! It works. (Finally.)
+ * This controller now works by making simple text, `message`, and a JavaScript object, `person`, available to the
+ * controller.
+ *
+ * It has an issue though. When the page is first rendered, the console reports an error:
+ *
+ * 	 GET file:///Users/larryjones/professional/projects/angular_js_getting_started/first_controller/%7B%7Bperson.imageSource%7D%7D net::ERR_FILE_NOT_FOUND
+ * 
+ * This error occurs because the browser renders the HTML page **before** AngularJS is completed loaded.
+ *
+ * I'll fix this issue in the next commit.
+ *
  */
